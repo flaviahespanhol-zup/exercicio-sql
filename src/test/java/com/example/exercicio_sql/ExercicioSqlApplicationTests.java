971c438bbbd62b2fc2bb4437555fcdca1e3e4509
flaviahespanhol-zup <flavia.hespanhol@zup.com.br>
2025-02-12 14:15:24 -0300

    feat: adiciona arquivos

---
+++ src/test/java/com/example/exercicio_sql/ExercicioSqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exercicio_sql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExercicioSqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
